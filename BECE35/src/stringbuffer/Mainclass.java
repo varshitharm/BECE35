fbb658a903df4e30f3ba7e9d8d51fc02e4820e0c
MADHU <MADHU@DELL-PC.mshome.net>
2020-04-01 11:23:07 +0530

    Stringbuffer

--- BECE35/src/stringbuffer/Mainclass.java
+++ BECE35/src/stringbuffer/Mainclass.java
@@ -12,15 +12,35 @@ public class Mainclass
 			System.out.println(res);
 		}
 		
-		return res;
-			
+		return res;		
 	}
+	
+	public static String test2(String str)
+	{
+		String res = "";//empty string
+		StringBuilder sb1 = new StringBuilder();//empty SB
+		
+		for(int i=0; i<=(str.length()-1)/2; i++)
+		{
+			sb1 = sb1.append(str.charAt(i));
+			System.out.println(sb1);
+		}
+		
+		res = sb1.toString();
+		return res;		
+	}
+	
+	
 	public static void main(String[] args) 
 	{
       String s1 = "aaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbcccccccccccccccccccc";
       System.out.println(test(s1));
+      System.out.println(test2(s1));
 	}
 }
+
+
+
 
 
 
