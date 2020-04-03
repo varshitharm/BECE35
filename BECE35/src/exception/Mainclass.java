cb6d1faab338cec845b3e90ea64b402cdfdd9f4a
MADHU <MADHU@DELL-PC.mshome.net>
2020-04-03 11:14:52 +0530

    try-cath blocks

--- BECE35/src/exception/Mainclass.java
+++ BECE35/src/exception/Mainclass.java
@@ -2,23 +2,34 @@ package exception;
 
 public class Mainclass 
 {
-	public static void test(int num)
+	public static void test(int num)//called
 	{
 	  System.out.println("test() starts...");
+	  int[] a1 = {20,40,60};
 	  
-	  int res = 50 / num; //50 / 0
-	  System.out.println("res = "+res);
+	  try
+	  {
+		System.out.println("array ele : "+a1[num]);
+	    int res = 50 / num; //50 / 0
+	    System.out.println("res = "+res);
+	  }
+	  catch(Exception e)
+	  {
+		e.printStackTrace();
+	  }
+	 
 	  
 	  System.out.println("hello world");
 	  
 	  System.out.println("test() ends...");
 	}
 	
-	public static void main(String[] args) 
+	public static void main(String[] args)//calling 
 	{
       System.out.println("main() starts...");
       
-      test(0);
+      test(5);
+      
       
       System.out.println("main() ends...");
 	}
